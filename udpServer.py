56942589228c33783a760e2c94ed1e0c3163901c
Juan Camilo Ruiz <jc.ruiz@uniandes.edu.co>
2018-11-19 07:34:30 -0500

    cambio

--- udpServer.py
+++ udpServer.py
@@ -53,7 +53,7 @@ def threaded_function(id, addr):
         i = i + 1
 
         # envia el mensaje de termino.
-        for i in range(intensity):
+        for j in range(intensity):
             serverSocket.sendto(outputData.encode(), addr)
 
         # imprime el contador de mensajes.
